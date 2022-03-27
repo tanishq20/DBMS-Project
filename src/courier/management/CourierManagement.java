e505508193e169ef88ce7fc189069acf4102b13f
Tanishq Kumar <66270248+tanishq20@users.noreply.github.com>
2022-03-27 20:06:47 +0530

    refactor: updated CourierManagement

--- src/courier/management/CourierManagement.java
+++ src/courier/management/CourierManagement.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package courier.management;
 
 import javafx.application.Application;
@@ -11,10 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Wipro
- */
 public class CourierManagement extends Application {
     
     @Override
@@ -27,9 +18,6 @@ public class CourierManagement extends Application {
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
